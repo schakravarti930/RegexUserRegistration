f7f8e3da5ae511973e505ca2cf9fb29a7ef0d2e1
schakravarti <schakravarti930@gmail.com>
2020-10-06 13:39:52 +0530

    [Swapnil] Add : Validation For First Name

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidation regexValidation = new RegexValidation();
+            Console.WriteLine("Enter Your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateFirstName(firstName));
+        }
+    }
+}
