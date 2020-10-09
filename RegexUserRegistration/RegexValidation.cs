bd111cc69835abab23c192f70b640ec85d4fc551
schakravarti <schakravarti930@gmail.com>
2020-10-09 14:33:01 +0530

    [Swapnil] Add : Validation for exactly one special character

--- RegexUserRegistration/RegexValidation.cs
+++ RegexUserRegistration/RegexValidation.cs
@@ -12,7 +12,7 @@ namespace RegexUserRegistration
         public const string REGEX_EMAIL = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public const string REGEX_MOBILENUMBER = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
         //  \S matches any non-whitespace character
-        public const string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*\\d)[\\S]{8,}$";
+        public const string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*\\d)(?=[\\w]*[\\W][\\w]*$)[\\S]{8,}$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME); 
