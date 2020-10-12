8b5ab88bdb2d76ce1f9565be1342d341712eab11
schakravarti <schakravarti930@gmail.com>
2020-10-12 19:58:49 +0530

    [Swapnil] Add : Unit Test for Each field

---
+++ UserRegistrationTest/UnitTest1.cs
@@ -0,0 +1,110 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexUserRegistration;
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private RegexValidation user;
+
+        [TestInitialize]
+        public void InitClassObject()
+        {
+            //Arrange
+             user = new RegexValidation();
+        }
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
+        {
+            string firstName = "Swapnil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
+        {
+            string lastName = "Chakravarti";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
+        {
+            string email = "schakravarti930@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
+        {
+            string mobileNumber = "91 9131942446";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            
+            string password = "Swapnil$232";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        //Sad Test Cases(Test Cases Fail The Entry)
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        {
+            string firstName = "swapnil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        {
+            string lastName = "chakravarti";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        {
+            string email = "Swapnil.Chakravarti.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        {
+            string mobileNumber = "91 0876543210";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        {
+            string password = "swapnil@123*";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
