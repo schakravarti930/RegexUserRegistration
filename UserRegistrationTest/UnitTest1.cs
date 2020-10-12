93acd682f4eec144e501dd5834b5b485dfb8f8a3
schakravarti <schakravarti930@gmail.com>
2020-10-12 20:15:04 +0530

    [Swapnil] Add : Parametrized Unit Test for Validating Emails

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -106,5 +106,19 @@ namespace UserRegistrationTest
             //Assert
             Assert.IsFalse(result);
         }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateEmailId_Should_return_true(string email)
+        {
+            Assert.IsTrue(user.ValidateEmail(email));
+        }
     }
 }
